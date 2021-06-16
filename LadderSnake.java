7e821e8e2f5b962ea18fef99810b6ac85a7e822d
Minal <patilminalp@gmail.com>
2021-06-16 23:45:15 +0530

    Added:The Player rolls the die to get a no. betn 1 to 6

--- LadderSnake.java
+++ LadderSnake.java
@@ -4,5 +4,11 @@ public class LadderSnake{
 		int Position=0;
 		System.out.println("Welcome to Snake-Ladder Program Problem");
 		System.out.println("You are at Starting Position::"+Position);
+		Die();
 	}
+	public static void Die(){
+		int DieCheck = (int) Math.floor(Math.random()*10)%6+1;
+		System.out.println("Dice Value::"+ DieCheck);
+		
+		}
 }
